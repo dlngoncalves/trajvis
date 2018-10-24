ea31c9109f466c28b842bf836cb2c0515a2ee2d1
dlngoncalves@gmail.com <dlngoncalves@gmail.com>
2018-10-24 13:57:09 -0300

    Initial Commit

---
+++ Waves/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Waves
+//
+//  Created by Diego Gonçalves on 24/10/18.
+//  Copyright © 2018 Diego Gonçalves. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
