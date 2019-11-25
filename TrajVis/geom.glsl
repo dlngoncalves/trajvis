00a459c6d49f9638e1764b80525e80dbbc5830e9
Diego Gonçalves <dlngoncalves@gmail.com>
2019-11-25 15:53:45 -0300

    deleted a bunch of commented out stuff, somethings were working for rendering different shapes but werent being used anymore, can look up in history if needed

--- TrajVis/geom.glsl
+++ TrajVis/geom.glsl
@@ -1,18 +1,7 @@
-
-
 #version 410
 
-//layout (points) in;
-//layout (lines) in;
 layout (lines_adjacency) in;
-//layout (triangle_strip, max_vertices = 4) out;
-
-//layout (points) in;
-//layout(line_strip, max_vertices = 4) out;
-
-//layout (points, max_vertices=4) out;
 layout (triangle_strip, max_vertices = 4) out;
-//layout (points, max_vertices = 4) out;
 
 in vec3 vertColorTemp_g[];
 in float speed_g[];
@@ -40,17 +29,10 @@ void main() {
         color = vec3((speed_g[1]/averageSpeed),0.1,0.1);
     }
     
-    //vec3 speedColor = vec3((speed_g[0]/averageSpeed),0.1,0.1);
+    //need to work with elevation data here
+    float constantHeight = gl_in[1].gl_Position.y;
     
-    //i need to clean this shader
-    //or save working geometry generation to functions
-    
-    //must figure out why it is a bit view dependent
-    //probably should start the triangle on the vertex closest to the camera? doesnt make a lot of sense
-    
-    float constantHeight = gl_in[0].gl_Position.y;
-    
-    //normal of line segment
+    //normals of line segments
     float dx = gl_in[1].gl_Position.x - gl_in[0].gl_Position.x;
     float dz = gl_in[1].gl_Position.z - gl_in[0].gl_Position.z;
     vec2 normal1 = vec2(-dz,dx);
@@ -85,214 +67,42 @@ void main() {
     //miter1 *= length1;
     //miter2 *= length2;
     
-    //normal1 = vec2(-1.0,1.0);
-    //normal2 = vec2(1.0,-1.0);
-    //vec4 normal1 = gl_in[0].gl_Position;// + normal1Dist;
-    //vec4 normal2 = gl_in[1].gl_Position;// + normal2Dist;
-    
-    //normal1.x += normal1Dist.x;
-    //normal1.z += normal1Dist.y;
-    //normal2.x += normal2Dist.x;
-    //normal2.z += normal1Dist.y;
-//    for(float i = 0; i < 6.38 ; i+=0.1)  //generate vertices at positions on the circumference from 0 to 2*pi
-//    {
-//        vec4 newPos = vec4(gl_in[0].gl_Position.x+0.5*cos(i),gl_in[0].gl_Position.y,gl_in[0].gl_Position.z,1.0);   //circle parametric equation
-//
-//        gl_Position = projection_mat * view_mat * model_mat * newPos;
-//        vertColorTemp = vertColorTemp_g[0];
-//        EmitVertex();
-//    }
-    
-    
-//    for(float i = 0; i < 4 ; i+1)  //generate vertices at positions on the circumference from 0 to 2*pi
-//    {
-//        vec4 newPos = vec4(gl_in[0].gl_Position.x+0.5*cos(i),gl_in[0].gl_Position.y,gl_in[0].gl_Position.z,1.0);   //circle parametric equation
 
-//    for(int i = 0; i < gl_in.length (); i++) {
-//        vec4 newPos = vec4(gl_in[0].gl_Position.x, gl_in[0].gl_Position.y+(2*i),gl_in[0].gl_Position.z,1.0);
-//        vertColorTemp = vertColorTemp_g[i];
-//        gl_Position = projection_mat * view_mat * model_mat * newPos;
-//        EmitVertex();
-//
-//        newPos = vec4(gl_in[1].gl_Position.x, gl_in[1].gl_Position.y+(2*i),gl_in[1].gl_Position.z,1.0);
-//        vertColorTemp = vertColorTemp_g[i];
-//        gl_Position = projection_mat * view_mat * model_mat * newPos;
-//        EmitVertex();
-//    }
-    
-    
-//        float scaleFac = (speed_g[0]/247) * 5;
-//
-//        float colorFac = (speed_g[0]/247);
-//        mat4 scaleMat = scale(scaleFac,1.0,scaleFac);
-//        //model_mat *= scale;
-//        mat4 newmodel = model_mat * scaleMat;
-//
-//        vec3 newColor = vec3(colorFac,0.1,0.1);
-//
-//        vec4 newPos = vec4(gl_in[0].gl_Position.x - 1.0, gl_in[0].gl_Position.y,gl_in[0].gl_Position.z + 1.0,1.0);
-//        newPos.x -= scaleFac;
-//        newPos.z += scaleFac;
-//        gl_Position = projection_mat * view_mat * model_mat * newPos;
-////        vertColorTemp = vertColorTemp_g[0];
-//        vertColorTemp = newColor;
-//        EmitVertex();
-//
-//        newPos = vec4(gl_in[0].gl_Position.x - 1.0, gl_in[0].gl_Position.y,gl_in[0].gl_Position.z - 1.0,1.0);
-//        newPos.x -= scaleFac;
-//        newPos.z -= scaleFac;
-//        gl_Position = projection_mat * view_mat * model_mat * newPos;
-////        vertColorTemp = vertColorTemp_g[0];
-//        vertColorTemp = newColor;
-//        EmitVertex();
-//
-//        newPos = vec4(gl_in[0].gl_Position.x + 1.0, gl_in[0].gl_Position.y,gl_in[0].gl_Position.z + 1.0,1.0);
-//        newPos.x += scaleFac;
-//        newPos.z += scaleFac;
-//        gl_Position = projection_mat * view_mat * model_mat * newPos;
-////        vertColorTemp = vertColorTemp_g[0];
-//        vertColorTemp = newColor;
-//        EmitVertex();
-//
-//        newPos = vec4(gl_in[0].gl_Position.x + 1.0, gl_in[0].gl_Position.y,gl_in[0].gl_Position.z - 1.0,1.0);
-//        newPos.x += scaleFac;
-//        newPos.z -= scaleFac;
-//        gl_Position = projection_mat * view_mat * model_mat * newPos;
-////        vertColorTemp = vertColorTemp_g[0];
-//        vertColorTemp = newColor;
-//        EmitVertex();
-    
-    
-    //EndPrimitive();
-    
-    //gl_PointSize = speed_g[0] * 10;
-    //vec4 newPos = gl_in[0].gl_Position;
-    //vertColorTemp = vertColorTemp_g[0];
-    //gl_Position = projection_mat * view_mat * model_mat * newPos;
-    //EmitVertex();
-    //EndPrimitive();
-    
-//    vec3 speedColor = vec3((speed_g[0]/averageSpeed),0.1,0.1);
-//
     vec4 newPos = gl_in[2].gl_Position;
-//    newPos.x += 1.1;
-    //newPos.x -= 0.1;// + (speed_g[1]/averageSpeed);
-    //newPos.z +=0.1;
     newPos.x -= miter2.x;// * length2;
     newPos.z -= miter2.y;// * length2;
 //    newPos *= length2;
     newPos.y = constantHeight;
     gl_Position = projection_mat * view_mat * model_mat * newPos;
-//    //gl_Position = projection_mat * view_mat * model_mat * gl_in[1].gl_Position;
-//    //gl_Position.x -= 5;
-//    //gl_Position.z = constantHeight;
-//    //gl_Position.z -= 1;
     vertColorTemp = color;
-////    vertColorTemp = speedColor;
     EmitVertex();
-//    EndPrimitive();
-//
+
     newPos = gl_in[2].gl_Position;
     newPos.x += miter2.x;// * length2;
     newPos.z += miter2.y;// * length2;
 //    newPos *= length2;
-    //newPos.x -=0.1;// + (speed_g[0]/averageSpeed);
-    //newPos.z -=0.1;
     newPos.y = constantHeight;
     gl_Position = projection_mat * view_mat * model_mat * newPos;
-//    //gl_Position = projection_mat * view_mat * model_mat * gl_in[0].gl_Position;
-//    //gl_Position.x -= 5;
-//    //gl_Position.z = constantHeight;
-//    //gl_Position.z += 1;
     vertColorTemp = color;
-////    vertColorTemp = speedColor;
     EmitVertex();
-//    EndPrimitive();
-//
-//    //EndPrimitive();
-//
+
     newPos = gl_in[1].gl_Position;
-//    newPos.x += 1.1;
     newPos.x -= miter1.x;// * length1;;
     newPos.z -= miter1.y;// * length1;
 //    newPos *= length1;
-    //newPos = vec4(normal1.x,newPos.y,normal1.z,1.0);
-//    newPos.x += 0.1;// + (speed_g[1]/averageSpeed);
-//    newPos.z +=0.1;
     newPos.y = constantHeight;
     gl_Position = projection_mat * view_mat * model_mat * newPos;
-//    //gl_Position = projection_mat * view_mat * model_mat * gl_in[1].gl_Position;
-//    //gl_Position.x += 5;
-//    //gl_Position.z = constantHeight;
     vertColorTemp = color;
-////    vertColorTemp = speedColor;
     EmitVertex();
-//    EndPrimitive();
-//
+
     newPos = gl_in[1].gl_Position;
     newPos.x += miter1.x;// * length1;
     newPos.z += miter1.y;// * length1;
 //    newPos *= length1;
-    //newPos = vec4(normal2.x,newPos.y,normal2.z,1.0);
-//    newPos.x +=0.1;// + (speed_g[0]/averageSpeed);
-//    newPos.z -=0.1;
     newPos.y = constantHeight;
     gl_Position = projection_mat * view_mat * model_mat * newPos;
-//    //gl_Position = projection_mat * view_mat * model_mat * gl_in[0].gl_Position;
-//    //gl_Position.x += 5;
-//    //gl_Position.z = constantHeight;
     vertColorTemp = color;
-////    vertColorTemp = speedColor;
     EmitVertex();
     
-//
-//
-////    gl_Position.y += 1;//gl_in[0].gl_Position;
-////    vertColorTemp = vertColorTemp_g[0];
-////    EmitVertex();
-//
     EndPrimitive();
 }
-
-//// extension should be core -- commented out
-//// replaced gl_VerticesIn with gl_in.length ()
-////#extension GL_EXT_geometry_shader4 : enable
-//
-//layout (points) in;
-//// convert to points, line_strip, or triangle_strip
-//layout (triangle_strip, max_vertices = 4) out;
-//
-//
-//// NB: in and out pass-through vertex->fragment variables must go here if used
-//in vec3 colour[];
-//out vec3 f_colour;
-//
-//in vec3 position_eye;
-//in vec3 vertColorTemp;
-//
-//in vec3 position_eye;
-//in vec3 vertColorTemp;
-//
-//void main () {
-//    for(int i = 0; i < gl_in.length (); i++) {
-//        // use original point as first point in triangle strip
-//        gl_Position = gl_in[i].gl_Position;
-//        // output pass-through data to go to fragment-shader (colour)
-//        f_colour = colour[0];
-//        // finalise first vertex
-//        EmitVertex();
-//        // create another point relative to the previous
-//        gl_Position.y += 0.4;
-//        f_colour = colour[0];
-//        EmitVertex();
-//        // create another point relative to the previous
-//        gl_Position.x += 0.2;
-//        gl_Position.y -= 0.4;
-//        f_colour = colour[0];
-//        EmitVertex();
-//        // create another point relative to the previous
-//        gl_Position.y += 0.4;
-//        f_colour = colour[0];
-//        EmitVertex();
-//    }
-//}
