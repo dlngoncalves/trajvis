ec58f4f2cd558b7c0b854fc3d45ac7d1001e9395
Diego Gonçalves <dlngoncalves@gmail.com>
2020-03-04 17:31:48 -0300

    Some stuff in the trajectorymanager

--- TrajVis/TrajectoryManager.h
+++ TrajVis/TrajectoryManager.h
@@ -6,17 +6,25 @@
 //  Copyright © 2019 Diego Gonçalves. All rights reserved.
 //
 
-#ifndef Trajectory_h
-#define Trajectory_h
+#ifndef TrajectoryManager_h
+#define TrajectoryManager_h
 
 #include <stdio.h>
+#include <vector>
+#include "TrajParser.h"
 
 //created this class because I wanted to migrate some stuff away from the trajparser class but I think I'm just gonna refactor that one
 class TrajectoryManager {
     
+    std::vector<TrajParser> trajlist; //this vector is currently on the TrajParser class itself, need to bring it here
+    //should have a large buffer for all traj data that is submitted to the gpu - separate from the trajparser vector
+    //more like a vec3 vector
+    //but ideally that should be a mapped buffer instead
+
     
 public:
     TrajectoryManager();
     ~TrajectoryManager();
 };
-#endif /* Trajectory_h */
+
+#endif /* TrajectoryManager_h */
